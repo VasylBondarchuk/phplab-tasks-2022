316702bdace7122d33f6382d1339bb70eefc561a
DESKTOP-CHITM9A\teorf <teorfiz@gmail.com>
2022-08-11 10:21:07 +0300

    GetUniqueFirstLettersTest refactoring

--- tests/GetUniqueFirstLettersTest.php
+++ tests/GetUniqueFirstLettersTest.php
@@ -13,15 +13,20 @@ class GetUniqueFirstLettersTest extends TestCase
      *
      */
     const DS = DIRECTORY_SEPARATOR;
+    const INCLUDE_FILE_RELATIVE_PATH = self::DS .'src'. self::DS . 'web'. self::DS .'functions.php';
 
     /**
      * @return void
      */
-    public function testPositive()
+    public function testPositive(): void
     {
-        require_once(getcwd() . self::DS .'src'. self::DS . 'web'. self::DS .'functions.php');
-        $actualResult = getUniqueFirstLetters(getAirports());
         $expectedResult = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y'];
-        $this->assertEquals($expectedResult, $actualResult);
+        $this->assertEquals($expectedResult, $this->getActualResult());
+    }
+
+    private function getActualResult(): array
+    {
+        require_once(getcwd() . self::INCLUDE_FILE_RELATIVE_PATH);
+        return getUniqueFirstLetters(getAirports());
     }
 }
